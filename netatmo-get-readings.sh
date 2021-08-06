a5b03e399e24c5427a2c336c7abcb0e9be0b90dc
Daniel Nisbet <daniel.nisbet@jisc.ac.uk>
2021-08-06 15:16:56 +0100

    Add rain guage

--- netatmo-get-readings.sh
+++ netatmo-get-readings.sh
@@ -15,4 +15,4 @@ https://api.netatmo.com/oauth2/token > netatmo-refresh.txt
 
 token=$(cat netatmo-refresh.txt | jq -r '.access_token')
 
-curl -s -X GET "https://api.netatmo.com/api/getstationsdata?get_favorites=false" -H "accept: application/json" -H "Authorization: Bearer $token" | jq -r '"Outdoor Temp: "+(.body.devices[0].modules[0].dashboard_data.Temperature|tostring)+ "c ("+(.body.devices[0].modules[0].dashboard_data.temp_trend|tostring)+")\nPressure: "+(.body.devices[0].dashboard_data.Pressure|tostring)+" [hPa]\nNoise: "+(.body.devices[0].dashboard_data.Noise|tostring)+"[dB]"'
+curl -s -X GET "https://api.netatmo.com/api/getstationsdata?get_favorites=false" -H "accept: application/json" -H "Authorization: Bearer $token" | jq -r '"Outdoor Temp: "+(.body.devices[0].modules[0].dashboard_data.Temperature|tostring)+ "[c] ("+(.body.devices[0].modules[0].dashboard_data.temp_trend|tostring)+")\nPressure: "+(.body.devices[0].dashboard_data.Pressure|tostring)+" [hPa]\nNoise: "+(.body.devices[0].dashboard_data.Noise|tostring)+"[dB]\nRain: "+(.body.devices[0].modules[1].dashboard_data.sum_rain_1|tostring)+"[mm/hr]"'
